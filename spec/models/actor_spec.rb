a19b975b25a9c7e7a07345b78c33ee303564c076
awosborn <alanturing@example.coclear>
2021-01-04 11:12:40 -0700

    Add average_age method and test

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -8,6 +8,15 @@ RSpec.describe Actor, type: :model do
   describe 'relationships' do
     it { should have_many :movie_actors}
     it { should have_many(:movies).through(:movie_actors)}
+    end
+
+  describe 'class methods' do 
+    it "should average age" do
+    bale = Actor.create!(name:"Christian Bale", age: 44)
+    man = Actor.create!(name:"genericman!", age: 26)
+
+    expect(Actor.average_age).to eq(35)
+    end
   end
-  end
+end
 end
